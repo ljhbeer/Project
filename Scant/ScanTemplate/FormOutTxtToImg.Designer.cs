f6a3ff436439ac194d2c0e126860b3546437b3f0
ljh_beer <ljh_beer@163.com>
2018-01-05 17:48:10 +0800

    ScanT v3.304 ..

--- Scant/ScanTemplate/FormOutTxtToImg.Designer.cs
+++ Scant/ScanTemplate/FormOutTxtToImg.Designer.cs
@@ -31,10 +31,9 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonOutSit = new System.Windows.Forms.Button();
             this.buttonOutNameList = new System.Windows.Forms.Button();
             this.buttonTxtToImg = new System.Windows.Forms.Button();
             this.listBox2 = new System.Windows.Forms.ListBox();
@@ -45,11 +44,12 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.checkBoxBlackFont = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -61,21 +61,20 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 3, 1);
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 3, 2);
             this.tableLayoutPanel1.Controls.Add(this.listBox2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 2, 1);
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 3, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.66019F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 47F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(682, 493);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
@@ -93,29 +92,16 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(303, 29);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(376, 405);
+            this.pictureBox1.Size = new System.Drawing.Size(376, 414);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.button5);
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.buttonOutNameList);
-            this.panel1.Controls.Add(this.buttonTxtToImg);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(303, 440);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(376, 30);
-            this.panel1.TabIndex = 5;
-            // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(302, 4);
+            this.button5.Location = new System.Drawing.Point(217, 3);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(65, 24);
+            this.button5.Size = new System.Drawing.Size(38, 38);
             this.button5.TabIndex = 4;
             this.button5.Text = "保存图片";
             this.button5.UseVisualStyleBackColor = true;
@@ -123,27 +109,28 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(244, 3);
+            this.button4.Location = new System.Drawing.Point(173, 3);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(65, 24);
+            this.button4.Size = new System.Drawing.Size(38, 38);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // buttonOutSit
             // 
-            this.button3.Location = new System.Drawing.Point(173, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(65, 24);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonOutSit.Location = new System.Drawing.Point(123, 3);
+            this.buttonOutSit.Name = "buttonOutSit";
+            this.buttonOutSit.Size = new System.Drawing.Size(44, 38);
+            this.buttonOutSit.TabIndex = 2;
+            this.buttonOutSit.Text = "输出座次";
+            this.buttonOutSit.UseVisualStyleBackColor = true;
+            this.buttonOutSit.Click += new System.EventHandler(this.buttonOutSit_Click);
             // 
             // buttonOutNameList
             // 
-            this.buttonOutNameList.Location = new System.Drawing.Point(95, 6);
+            this.buttonOutNameList.Location = new System.Drawing.Point(73, 3);
             this.buttonOutNameList.Name = "buttonOutNameList";
-            this.buttonOutNameList.Size = new System.Drawing.Size(72, 24);
+            this.buttonOutNameList.Size = new System.Drawing.Size(44, 38);
             this.buttonOutNameList.TabIndex = 1;
             this.buttonOutNameList.Text = "输出名单";
             this.buttonOutNameList.UseVisualStyleBackColor = true;
@@ -151,11 +138,11 @@
             // 
             // buttonTxtToImg
             // 
-            this.buttonTxtToImg.Location = new System.Drawing.Point(11, 4);
+            this.buttonTxtToImg.Location = new System.Drawing.Point(3, 3);
             this.buttonTxtToImg.Name = "buttonTxtToImg";
-            this.buttonTxtToImg.Size = new System.Drawing.Size(78, 24);
+            this.buttonTxtToImg.Size = new System.Drawing.Size(64, 38);
             this.buttonTxtToImg.TabIndex = 0;
-            this.buttonTxtToImg.Text = "输出到图片";
+            this.buttonTxtToImg.Text = "输出列表到图片";
             this.buttonTxtToImg.UseVisualStyleBackColor = true;
             this.buttonTxtToImg.Click += new System.EventHandler(this.buttonTxtToImg_Click);
             // 
@@ -166,7 +153,7 @@
             this.listBox2.ItemHeight = 12;
             this.listBox2.Location = new System.Drawing.Point(103, 29);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(94, 405);
+            this.listBox2.Size = new System.Drawing.Size(94, 414);
             this.listBox2.TabIndex = 2;
             // 
             // label2
@@ -184,7 +171,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(3, 29);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(94, 405);
+            this.panel2.Size = new System.Drawing.Size(94, 414);
             this.panel2.TabIndex = 6;
             // 
             // listBoxScantData
@@ -212,7 +199,7 @@
             this.textBox1.Location = new System.Drawing.Point(203, 29);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(94, 405);
+            this.textBox1.Size = new System.Drawing.Size(94, 414);
             this.textBox1.TabIndex = 7;
             // 
             // flowLayoutPanel1
@@ -234,6 +221,19 @@
             this.checkBoxBlackFont.Text = "黑色字体";
             this.checkBoxBlackFont.UseVisualStyleBackColor = true;
             // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.buttonTxtToImg);
+            this.flowLayoutPanel2.Controls.Add(this.buttonOutNameList);
+            this.flowLayoutPanel2.Controls.Add(this.buttonOutSit);
+            this.flowLayoutPanel2.Controls.Add(this.button4);
+            this.flowLayoutPanel2.Controls.Add(this.button5);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(303, 449);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(376, 41);
+            this.flowLayoutPanel2.TabIndex = 9;
+            // 
             // FormOutTxtToImg
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -246,10 +246,10 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -262,9 +262,8 @@
         private System.Windows.Forms.ListBox listBoxScantData;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonOutSit;
         private System.Windows.Forms.Button buttonOutNameList;
         private System.Windows.Forms.Button buttonTxtToImg;
         private System.Windows.Forms.Button button5;
@@ -273,5 +272,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.CheckBox checkBoxBlackFont;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
     }
 }
--- Scant/ScanTemplate/FormOutTxtToImg.cs
+++ Scant/ScanTemplate/FormOutTxtToImg.cs
@@ -76,10 +76,8 @@ namespace ScanTemplate
             ScanData sd = (ScanData)listBoxScantData.SelectedItem;
             if (File.Exists(sd.DataFullName))
             {
+                //ScanDataStudents sds = new ScanDataStudents(sd.DataFullName);
                 string[] ls = File.ReadAllLines(sd.DataFullName);
-                List<string> titles = ls[0].Split(',').ToList();
-
-                //StringBuilder sb = new StringBuilder()
                 List<string> list = new List<string>();
                 List<int> kh = new List<int>();
                 list.Add("已交学生名单");
@@ -121,5 +119,113 @@ namespace ScanTemplate
                  .Select(r1 => r1.Name).ToList();
             return namelist;
         }
+
+        private void buttonOutSit_Click(object sender, EventArgs e)
+        {
+            if (listBoxScantData.SelectedIndex == -1) return;
+            ScanData sd = (ScanData)listBoxScantData.SelectedItem;
+            if (File.Exists(sd.DataFullName))
+            {
+                ScanDataStudents sds = new ScanDataStudents(sd.DataFullName);
+                List<List<string>> list = sds.GetZC(10,10);
+                string str = string.Join("\r\n",
+                    list.Select( r => string.Join("\t",r)).ToList())
+                    +"\r\n===\r\n" + string.Join("\r\n",sds.ErrSeat);
+                textBox1.Text = str;
+            }
+        }
+    }
+    public class ScanDataStudents
+    { 
+        public ScanDataStudents(string filename)
+        {
+            string[] ls = File.ReadAllLines(filename);
+            string title = ls[0];
+            Students = ls.Skip(1).Select(r => new ScanDataStudent(r,title)).ToList();      
+            ErrSeat =  new List<string>();
+        }
+        public List<List<string>> GetZC(int ColMax,int RowMax)
+        {
+            List<List<string>> ll = new List<List<string>>();
+            for (int i = 0; i < ColMax; i++)
+            {
+                List<string> l = new List<string>();
+                for (int j = 0; j < RowMax; j++)
+                    l.Add("");
+                    ll.Add(l);
+            }
+            ErrSeat.Clear(); 
+            foreach (ScanDataStudent s in Students)
+            {
+                if (s.RowIndex >= 0 && s.ColIndex >= 0 && s.RowIndex < RowMax && s.ColIndex < ColMax)
+                {
+                    if( ll[s.ColIndex][s.RowIndex] == "")
+                        ll[s.ColIndex][s.RowIndex] = s.Name;
+                    else
+                        ll[s.ColIndex][s.RowIndex] += s.Name;
+                }
+                else
+                {
+                    ErrSeat.Add(s.Name + s.SKH);
+                }
+            }
+            return ll;
+        }
+
+        public List<ScanDataStudent> Students { get; set; }
+
+        public List<string> ErrSeat { get; set; }
+    }
+    public class ScanDataStudent
+    {
+        public ScanDataStudent(string s,string title)
+        {
+            string[] ss = s.Split(',');
+            this.State =true;
+            this.KH = 0;
+            this.RowIndex = -1;
+            this.ColIndex = -1;
+            if (ss.Length < 5)
+            {
+                this.State = false;
+                this.Str  = s;
+                return;
+            }
+            this.Name = ss[4];
+            if (title.Contains("考号") && !ss[3].Contains("-") && ss[3] != "")
+            {
+                try
+                {
+                    this.KH = Convert.ToInt32(ss[3].Trim());
+                }
+                catch { }
+            }
+            if (title.Contains("自定义") && !ss[6].Contains("-") && ss[6] != "")
+            {
+                try
+                {
+                    string[] rc = ss[6].Split(new string[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
+                    if (rc.Length == 2)
+                    {
+                        this.RowIndex = Convert.ToInt32(rc[0]);
+                        this.ColIndex = Convert.ToInt32(rc[1]);
+                    }
+                }
+                catch { }
+            }
+            else
+            {
+                this.SKH = ss[6].Replace("|","");
+            }
+        }
+        
+        public string Str { get; set; }
+        public bool State { get; set; }
+        public string Name { get; set; }
+        public int KH { get; set; }
+        public int ColIndex { get; set; }
+        public int RowIndex { get; set; }
+
+        public string SKH { get; set; }
     }
 }
